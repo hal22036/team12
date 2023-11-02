61584ee64bc35e54b89915a183c3c749bec005d7
Jeni Hales <hal22036@byui.edu>
2023-11-02 11:35:17 -0700

    fix broken cart

--- src/js/product.js
+++ src/js/product.js
@@ -3,9 +3,13 @@ import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// add product to the cart in localStorage
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let activeCart = JSON.parse(localStorage.getItem("so-cart")) || [];
+  activeCart.push(product);
+  setLocalStorage("so-cart", activeCart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
