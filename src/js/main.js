fd39999f8a52d437d3e93026cdc8207278b7c820
marvellouschirunga <marvellouschirunga@gmail.com>
2023-11-09 16:20:04 +0200

    Added customizable alert to index.html. This feauture adds the ability to quickly add an informative alert to the product listing page to make announcements.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 import productData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import { setLocalStorage } from "./utils.mjs";
+import Alert from "./Alert";
 
 const productDataInstance = new productData("tents");
 const element = document.querySelector(".product.list");
 const productList = new ProductListing("Tents", productDataInstance, element);
 productList.init();
+
+new Alert();
 
 export function updateIcon() {
   let activeCart = JSON.parse(localStorage.getItem("so-cart")) || [];
