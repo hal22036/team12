bb00535c4c4183a13cb8a92f6bc064b9575dd408
ronaldoamonteiro <ron.monteiro07@gmail.com>
2023-11-19 10:52:20 -0300

    feat: week 03 && chore: console.log removal

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -11,27 +11,17 @@ async function convertToJson(res) {
 
 export default class ProductData {
   constructor(category) {
-    // console.log({category})
     // this.category = category;
     // this.path = `/json/${this.category}.json`;
   }
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
-    // const response = await fetch(`http://server-nodejs.cit.byui.edu:3000/products/search/${category}`);
     const data = await convertToJson(response);
-    // console.log({data})
     return data.Result;
-    // return data.Result;
-    // return fetch(this.path)
-    //   .then(convertToJson)
-    //   .then((data) => data);
   }
   async findProductById(id) {
     const response = await fetch(baseURL + `product/${id}`);
-    // const response = await fetch(`http://server-nodejs.cit.byui.edu:3000/product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
-    // const products = await this.getData();
-    // return products.find((item) => item.Id === id);
   }
 }
