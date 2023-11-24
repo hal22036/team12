c5724aa4ae830a1aa8c0d9b301661ca3fcd9a8ee
ronaldoamonteiro <ron.monteiro07@gmail.com>
2023-11-24 14:01:29 -0300

    fix: adding signup page in the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         product: resolve(__dirname, "src/product_pages/index.html"),
         productListing: resolve(__dirname, "src/product-listing/index.html"),
         success: resolve(__dirname, "src/checkout/success.html"),
+        signup: resolve(__dirname, "src/signup.html"),
       },
     },
   },
